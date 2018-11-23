fdbf0e67cfc4f79099330168e75dc61f154caf65
Anthony Scinocco <scinocco.a@gmail.com>
2018-11-23 09:59:02 -0500

    Began adding queries and mutations. Updated prisma db schema

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,3 +1,8 @@
-const Mutations = {};
+const Mutations = {
+  createItem(parent, args, ctx, info) {
+    // TODO: check if they are logged in
+    const item = ctx.db.mutation.createItem({ data: { ...args } });
+  }
+};
 
 module.exports = Mutations;
