b9ebc84ebc41947e9f75b420271aa340358c2138
Anthony Scinocco <scinocco.a@gmail.com>
2018-11-29 22:11:53 -0500

    added update item support

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -3,6 +3,17 @@ const Mutations = {
     // TODO: check if they are logged in
     const item = await ctx.db.mutation.createItem({ data: { ...args } }, info);
     return item;
+  },
+  updateItem(parent, args, ctx, info) {
+    // this syntax takes a copy instead of a reference I believe
+    const updates = { ...args };
+    delete updates.id;
+    return ctx.db.mutation.updateItem({
+      data: updates,
+      where: {
+        id: args.id,
+      },
+    }, info);
   }
 };
 
