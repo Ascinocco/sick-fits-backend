33106494bfac64e3612ba826fbe5d2472f556353
Anthony Scinocco <scinocco.a@gmail.com>
2018-12-06 09:11:54 -0500

    implemented express middleware to get userId from jwt token and then pass it along in the req object. Also implement the get user aka 'me' query

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 require('dotenv').config({ path: 'variables.env' });
 const createServer = require('./createServer');
 const db = require('./db');
 
 const server = createServer();
 
-// TODO: Use experess middleware to handle cookiers (JWT)
 server.express.use(cookieParser());
-// TODO: Use express middleware to populate current user
+
+// decode jwt so we can get the user on every request
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    // send the user id along with the request
+    req.userId = userId;
+  }
+  next();
+});
 
 server.start({
   cors: {
