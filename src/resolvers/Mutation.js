1e39e10fb27c3665b17b7f4c20d0e9d938e19c64
Anthony Scinocco <scinocco.a@gmail.com>
2018-12-06 19:57:47 -0500

    added signout functionality

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -75,6 +75,11 @@ const Mutations = {
     // return user
     return user;
   },
+
+  signout(parent, args, ctx, info) {
+    ctx.response.clearCookie('token');
+    return { message: 'Goodbye!' };
+  }
 };
 
 module.exports = Mutations;
