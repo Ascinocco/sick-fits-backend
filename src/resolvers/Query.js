c25589753e7221a56a8bdb8b583ef19bafe05d16
Anthony Scinocco <scinocco.a@gmail.com>
2018-12-11 09:47:41 -0500

    added order query to get the order info for the order page on the frontend

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -26,6 +26,22 @@ const Query = {
 
     // query all users
     return await ctx.db.query.users({}, info);
+  },
+  async order(parent, args, ctx, info) {
+    if (!ctx.request.userId) {
+      throw new Error('You arent logged in!');
+    }
+
+    const order = await ctx.db.query.order({
+      where: { id: args.id },
+    }, info);
+
+    const ownsOrder = order.user.id === ctx.request.userId;
+    const hasPermissionToSeeOrder = ctx.request.user.permissions.includes('ADMIN');
+    if (!ownsOrder || !hasPermissionToSeeOrder) {
+      throw new Error('You cant see this budd');
+    }
+    return order;
   }
 };
 
